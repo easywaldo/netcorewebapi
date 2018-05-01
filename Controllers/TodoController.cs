9286beee3e2db1858df85fbddb3e6d6e44778fb2
jinam.lee <acetious@gmail.com>
2018-05-01 11:53:08 +0900

    first commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem
+                {
+                    Name = "Item1"
+                });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        [Route("getList")]
+        public IEnumerable<TodoItem> GetAll()
+        {            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetToDo")]
+        [Route("getbyid/{id}")]
+        public IActionResult GetById(long id)
+        {            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace TodoApi
+{
+    public class Program
+    {
+        //public static void Main(string[] args)
+        //{
+        //    BuildWebHost(args).Run();
+        //}   
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
